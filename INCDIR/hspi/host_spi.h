8852d95ce5f77ca13b2974de1977424a92531a4f
fduan <dfxhappy@163.com>
2019-04-27 11:05:49 +0800

    add host spi driver

--- INCDIR/hspi/host_spi.h
+++ INCDIR/hspi/host_spi.h
@@ -1,5 +1,21 @@
 #ifndef HOST_SPI_H
 #define HOST_SPI_H
 
+typedef enum spi_msg_sts_t {
+	HOST_SPI_MSG_INIT = 0,
+	HOST_SPI_MSG_INP, /* inprogress */
+	HOST_SPI_MSG_DONE,
+	HOST_SPI_MSG_INVALID,
+	HOST_SPI_MSG_FAIL
+} spi_msg_sts_t;
+
+typedef struct spi_msg_t {
+	UINT16 tx_len;
+	UINT16 rx_len;
+	UINT8  tx_done;
+	UINT8  rx_done;
+	UINT8 *tx_buf;
+	UINT8 *rx_buf;
+} spi_msg_t;
 
 #endif
--- SRCDIR/spi/host_spi.c
+++ SRCDIR/spi/host_spi.c
@@ -1,32 +1,24 @@
-#include "host_spi.h"
-
 #include "DSP28_Device.h"
 #include "boardcfg.h"
 #include "F2812_datatype.h"
+#include "host_spi.h"
 
 #define HOST_SPI_MAX_XFER_BYTES           16 /* 16 words per fifo */
 
-typedef struct spi_msg_t {
-	UINT8 tx_len;
-	UINT8 rx_len;
-	UINT8 *tx_buf;
-	UINT8 *rx_buf;
-} spi_msg_t;
-
 void HostSpiInit(void) {
-	   UINT16 regval;
+	UINT16 regval;
 
     // Initialize SPI-A:
     // Initialize SCI-A:
-    EALLOW;
-    regval = GpioMuxRegs.GPFMUX.all;
-    regval |= SPI_PIN_MUX;
-    GpioMuxRegs.GPFMUX.all = regval;
-    EDIS;
+	EALLOW;
+	regval = GpioMuxRegs.GPFMUX.all;
+	regval |= SPI_PIN_MUX;
+	GpioMuxRegs.GPFMUX.all = regval;
+	EDIS;
     /* loopback   8 bit data */
-    SpiaRegs.SPICCR.all = SPICCR_CFG;
+	SpiaRegs.SPICCR.all = SPICCR_CFG;
 
-    SpiaRegs.SPICTL.all = SPICTL_CFG;
+	SpiaRegs.SPICTL.all = SPICTL_CFG;
 
     SpiaRegs.SPIBRR = SPIBRR_CFG;//
 
@@ -43,18 +35,6 @@ void HostSpiDeassertCS(void) {
 
 }
 
-int HostSpiRead(UINT8* buffer, UINT16 length) {
-	HostSpiAssertCS();
-	HostSpiDeassertCS();
-	return 0;
-}
-
-int HostSpiWrite(UINT8* buffer, UINT16 length) {
-	HostSpiAssertCS();
-	HostSpiDeassertCS();
-	return 0;
-}
-
 void HostSpiReset(void) {
 	/* Disable SPI Master */
 	SpiaRegs.SPICCR.all = 0x0;
@@ -64,31 +44,123 @@ void HostSpiReset(void) {
 
 	SpiaRegs.SPICCR.all = SPICCR_CFG;
 	SpiaRegs.SPICTL.all = SPICTL_CFG;
-    SpiaRegs.SPIBRR = SPIBRR_CFG;
-    SpiaRegs.SPICCR.all = (0x80 | SPICCR_CFG);
+	SpiaRegs.SPIBRR = SPIBRR_CFG;
+	SpiaRegs.SPICCR.all = (0x80 | SPICCR_CFG);
 
     SpiaRegs.SPIFFRX.all = ( 1 << 13) | 0xF; /* no interrupt */
-    SpiaRegs.SPIFFTX.all = ( 1 << 15) | (1 << 13);
+	SpiaRegs.SPIFFTX.all = ( 1 << 15) | (1 << 13);
 
 	return ;
 }
 
-int HostSpiSetupXfer(spi_msg_t *spi_msg) {
-	/* reset SPI controller */
-	HostSpiReset();
-	/* Setup the TX FIFO and RX FIFO */
-	if( spi_msg->tx_len < HOST_SPI_MAX_XFER_BYTES ) {
-		SpiaRegs.SPIFFTX.all |= spi_msg->tx_len;
-	} else {
-		SpiaRegs.SPIFFTX.all |= 0xF;
+int HostSpiXferWrite(spi_msg_t *spi_msg) {
+	int  i = 0;
+	if( spi_msg->tx_len > 0 ) {
+		/* Setup the TX FIFO and RX FIFO */
+		if( spi_msg->tx_len < HOST_SPI_MAX_XFER_BYTES ) {
+			SpiaRegs.SPIFFTX.all |= spi_msg->tx_len;
+		} else {
+			SpiaRegs.SPIFFTX.all |= 0xF; /* 16 words */
+		}
+		spi_msg->tx_done = HOST_SPI_MSG_INP;
+		for( i = 0; i < spi_msg->tx_len; ++i ) {
+			SpiaRegs.SPITXBUF = spi_msg->tx_buf[i];
+		}
 	}
 
+	spi_msg->tx_done = HOST_SPI_MSG_DONE;
 	return 0;
 }
 
-int HostSpiXfer(spi_msg_t *spi_msg) {
+int HostSpiXferRead(spi_msg_t *spi_msg) {
+	int i = 0;
+	if( spi_msg->rx_len > 0 ) {
+		spi_msg->rx_done = HOST_SPI_MSG_INP;
+		for( i = 0; i < spi_msg->rx_len; ++i ) {
+			SpiaRegs.SPITXBUF = 0xFF; /* dummy byte */
+			while(SpiaRegs.SPIFFRX.bit.RXFFST !=1) {};
+			spi_msg->rx_buf[i] = SpiaRegs.SPIRXBUF;
+		}
+	}
+
+	spi_msg->rx_done = HOST_SPI_MSG_DONE;
+	return 0;
+}
+
+int HostSpiSetupXfer(spi_msg_t *spi_msg) {
+	int i = 0;
+	int loop = 0;
+	int remain = 0;
+	spi_msg_t cur_msg;
+
+	/* reset SPI controller */
+	HostSpiReset();
+	if( spi_msg->tx_buf ) {
+		loop = spi_msg->tx_len / HOST_SPI_MAX_XFER_BYTES;
+		remain = spi_msg->tx_len % HOST_SPI_MAX_XFER_BYTES;
+	/* tx 1st then rx */
+		for( i = 0; i < loop; ++i ) {
+			cur_msg.tx_len = HOST_SPI_MAX_XFER_BYTES;
+			cur_msg.tx_done = HOST_SPI_MSG_INIT;
+			cur_msg.tx_buf = spi_msg->tx_buf + (i * HOST_SPI_MAX_XFER_BYTES);
+			HostSpiXferWrite(&cur_msg);
+		}
+
+		if( remain > 0 ) {
+			cur_msg.tx_len = remain;
+			cur_msg.tx_done = HOST_SPI_MSG_INIT;
+			cur_msg.tx_buf = spi_msg->tx_buf + (i * HOST_SPI_MAX_XFER_BYTES);
+			HostSpiXferWrite(&cur_msg);
+		}
+	}
+	
+	if( spi_msg->rx_buf ) {
+		loop = spi_msg->rx_len / HOST_SPI_MAX_XFER_BYTES;
+		remain = spi_msg->rx_len % HOST_SPI_MAX_XFER_BYTES;
+
+		/* tx 1st then rx */
+		for( i = 0; i < loop; ++i ) {
+			cur_msg.rx_len = HOST_SPI_MAX_XFER_BYTES;
+			cur_msg.rx_done = HOST_SPI_MSG_INIT;
+			cur_msg.rx_buf = spi_msg->rx_buf + (i * HOST_SPI_MAX_XFER_BYTES);
+			HostSpiXferRead(&cur_msg);
+		}
+
+		if( remain > 0 ) {
+			cur_msg.rx_len = remain;
+			cur_msg.rx_done = HOST_SPI_MSG_INIT;
+			cur_msg.rx_buf = spi_msg->rx_buf + (i * HOST_SPI_MAX_XFER_BYTES);
+			HostSpiXferRead(&cur_msg);
+		}
+	}
+	return 0;
+}
+
+int HostSpiXferMsg(spi_msg_t *spi_msg) {
 	HostSpiAssertCS();
+	HostSpiSetupXfer(spi_msg);
 	HostSpiDeassertCS();
+	return 0;
+}
+
+int HostSpiXfer(UINT8* tx_buf, UINT16 tx_len, UINT8* rx_buf, UINT16 rx_len) {
+	spi_msg_t cur_msg;
+	cur_msg.tx_len = tx_len;
+	cur_msg.tx_buf = tx_buf;
+	cur_msg.rx_len = rx_len;
+	cur_msg.rx_buf = rx_buf;
+	HostSpiXferMsg(&cur_msg);
+	return 0;
+}
+
+
+int HostSpiRead(UINT8* buffer, UINT16 length) {
+	HostSpiXfer(NULL, 0, buffer, length);
+	return 0;
+}
+
+int HostSpiWrite(UINT8* buffer, UINT16 length) {
+	HostSpiXfer(buffer, length, NULL, 0);
 	return 0;
 }
 
