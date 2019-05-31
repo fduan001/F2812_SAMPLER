536d1b5a828d42c47d3eeca59e448216fb9eb254
fduan001 <dfxhappy@163.com>
2019-05-31 15:43:36 +0800

    add print the last byte for input register

--- SRCDIR/ad5684/AD568X.c
+++ SRCDIR/ad5684/AD568X.c
@@ -198,12 +198,7 @@ void AD568X_SetInputRegister(UINT32 registerValue)
     registerWord[1] = dataPointer[1];
     registerWord[2] = dataPointer[0];
     
-    PRINTF("0x%02x 0x%02x 0x%02x\n", registerWord[0], registerWord[1], registerWord[2]);
-    PRINTF("input: 0x%02x 0x%02x 0x%02x 0x%02x \n", (registerValue & 0xFF),
-    	((registerValue >> 8) & 0xFF),
-    	((registerValue >> 16) & 0xFF),
-    	((registerValue >> 24) & 0xFF)
-    	);
+    PRINTF("0x%02x 0x%02x 0x%02x\n", registerWord[0], registerWord[1], registerWord[2], registerWord[3]);
     SPI_Write(AD568X_SLAVE_ID, registerWord, 3);
 }
 
