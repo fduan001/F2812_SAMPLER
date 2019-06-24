a89cc6f3fccf4e33dbc679f1270a95b482fde7c6
fduan <dfxhappy@163.com>
2019-06-24 21:53:58 +0800

    move calibration before init IDAC

--- SRCDIR/temp/temp_measure.c
+++ SRCDIR/temp/temp_measure.c
@@ -97,6 +97,11 @@ int TempMeasInit(void) {
 
 	TempMeasStart();
 	PlatformDelay(20);
+	ADS1248_SetPGAGainAndDataRate(ADS1248_GAIN_4, ADS1248_DR_320);
+	TempSelfCalibration();
+
+	TempMeasStart();
+	PlatformDelay(20);
 	ADS1248_SetInputChan(ADS_P_AIN1, ADS_N_AIN2);
 	ADS1248_SetReference(0x20, 0x10);
 	PlatformDelay(10);
@@ -106,7 +111,7 @@ int TempMeasInit(void) {
 
 	TempMeasStop();
 	PlatformDelay(50000);
-	TempSelfCalibration();
+
 
 	return 0;
 }
